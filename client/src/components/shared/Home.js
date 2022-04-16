312c6773d8b2aabf013092d44f9adc8c0f78e047
Henry Doan <henrydoan96@gmail.com>
2022-04-16 09:58:43 -0600

    initial and auth

---
+++ client/src/components/shared/Home.js
@@ -0,0 +1,5 @@
+const Home = () => (
+  <h3>Starter Auth app</h3>
+)
+
+export default Home;
---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import AuthProvider from "./providers/AuthProvider";
+import { initMiddleware } from 'devise-axios';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+initMiddleware();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
